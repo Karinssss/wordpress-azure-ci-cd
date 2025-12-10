b7435b57ce5fc9d601578f56e4b4df654802c5f8
Karinssss <141112932+Karinssss@users.noreply.github.com>
2025-12-10 10:38:17 -0500

    Delete custom/index.php

--- custom/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Encabezado visible para demostrar el despliegue automatizado
-echo "<h1 style='color:red; text-align:center; margin-top:40px;'>
-  WordPress desplegado por pipeline CI/CD (build: " . date('Y-m-d H:i:s') . ")
-</h1>";
-
-// Llamamos al WordPress real
-require __DIR__ . '/../wp-blog-header.php';
